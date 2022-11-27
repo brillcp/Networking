f6e58036a44dfa66ab4728140e9574a85b9beb11
VG <viktor@gidlof.se>
2022-11-27 02:00:24 +0100

    Make properties public

--- Sources/Networking/HTTP/Header.swift
+++ Sources/Networking/HTTP/Header.swift
@@ -10,14 +10,14 @@ import Foundation
 public extension HTTP.Header {
     /// Header field values for HTTP requests
     enum Field {
-        static let urlEncoded = "application/x-www-form-urlencoded"
-        static let octetStream = "application/octet-stream"
-        static let contentLength = "Content-Length"
-        static let contentType = "Content-Type"
-        static let json = "application/json"
-        static let auth = "Authorization"
-        static let bearer = "Bearer %@"
-        static let basic = "Basic %@"
-        static let accept = "Accept"
+        public static let urlEncoded = "application/x-www-form-urlencoded"
+        public static let octetStream = "application/octet-stream"
+        public static let contentLength = "Content-Length"
+        public static let contentType = "Content-Type"
+        public static let json = "application/json"
+        public static let auth = "Authorization"
+        public static let bearer = "Bearer %@"
+        public static let basic = "Basic %@"
+        public static let accept = "Accept"
     }
 }
