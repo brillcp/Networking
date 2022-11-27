c0dd2b6ea031069b6bb702f5d4eadb9c6c67c654
VG <viktor@gidlof.se>
2022-11-27 02:10:33 +0100

    Make function open

--- Sources/Networking/ServerConfig/ServerConfig.swift
+++ Sources/Networking/ServerConfig/ServerConfig.swift
@@ -21,7 +21,7 @@ open class ServerConfig {
     /// Create a HTTP header for the requests. Defaults to `[Content-Type: application/json]`
     /// - parameter request: The given request to set up the header with
     /// - returns: A new `HTTP.Header` dictionary
-    public func header(forRequest request: Requestable) -> HTTP.Header {
+    open func header(forRequest request: Requestable) -> HTTP.Header {
         guard let contentType = request.contentType else { return HTTP.Header() }
         return [HTTP.Header.Field.contentType: contentType]
     }
