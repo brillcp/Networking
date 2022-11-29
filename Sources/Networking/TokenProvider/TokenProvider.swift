df0a078fcff389a9a59503ffe2717f39c60b12ea
VG <viktor@gidlof.se>
2022-11-29 18:09:31 +0100

    Improve copy

--- Sources/Networking/TokenProvider/TokenProvider.swift
+++ Sources/Networking/TokenProvider/TokenProvider.swift
@@ -15,9 +15,9 @@ public enum TokenProviderError: Error {
 // MARK: -
 /// A protocol for getting and setting JWT tokens
 public protocol TokenProvider: AnyObject {
-    /// The JWT token for the logged in user
+    /// A result object containing a token used in HTTP Authorization request headers
     var token: Result<String, TokenProviderError> { get }
-    /// Persist the given token to `UserDefaults` (could be any persistant storage)
+    /// Persist the given token to any persistant storage on device. E.g `UserDefaults`, `CoreData`, `Keychain`.
     /// - parameter token: The token to persist
     func setToken(_ token: String)
     /// Remove the token from the persistant storage
