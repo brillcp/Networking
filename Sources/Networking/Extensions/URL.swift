0ea37c52f325cb68f4841bd05480a482b1eba486
VG <viktor@gidlof.se>
2022-12-01 16:49:18 +0100

    Add URL extension

---
+++ Sources/Networking/Extensions/URL.swift
@@ -0,0 +1,20 @@
+//
+//  URL.swift
+//  
+//
+//  Created by Viktor Gidlöf on 2022-12-01.
+//
+
+import Foundation
+
+extension URL {
+
+    /// Creat a ´[String: String]´ dictionary from the query parameters of a URL
+    /// - returns: A new dictionary with the query items
+    func queryParameters() -> [String: String] {
+        guard let components = URLComponents(url: self, resolvingAgainstBaseURL: true),
+              let queryItems = components.queryItems
+        else { return [:] }
+        return queryItems.reduce(into: [String: String]()) { (result, item) in result[item.name] = item.value }
+    }
+}
