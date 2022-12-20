9127ce0b9115b555dd7cef8770f5cfeb3a5d2b20
VG <viktor@gidlof.se>
2022-12-20 23:48:09 +0100

    Small clean up

--- Networking-Example/Networking-Example/AppDelegate.swift
+++ Networking-Example/Networking-Example/AppDelegate.swift
@@ -10,12 +10,10 @@ import UIKit
 @main
 final class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         true
     }
 
-    // MARK: UISceneSession Lifecycle
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
